9bb14d749239d52154682500ee2d8c3076af1dac
anujeditbyanuj-cpu <anujeditbyanuj@gmail.com>
2026-05-22 07:51:41 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,45 +1,35 @@
 class Translation(object):
-    START_TEXT = """
+
+START_TEXT = """
+
 👋 Hᴇʟʟᴏ {} ✨
 
-╔══════════════════════╗
-🚀 Vɪᴘ Uʀʟ Uᴘʟᴏᴀᴅᴇʀ Bᴏᴛ 🚀
-╚══════════════════════╝
+╔══════════════════════╗ 🚀 Vɪᴘ Uʀʟ Uᴘʟᴏᴀᴅᴇʀ Bᴏᴛ 🚀 ╚══════════════════════╝
 
-⚡ Aᴅᴠᴀɴᴄᴇᴅ Tᴇʟᴇɢʀᴀᴍ Dɪʀᴇᴄᴛ Lɪɴᴋ Uᴘʟᴏᴀᴅᴇʀ
-🎬 Sᴛʀᴇᴀᴍ • Dᴏᴡɴʟᴏᴀᴅ • Uᴘʟᴏᴀᴅ Iɴsᴛᴀɴᴛʟʏ
-📥 Sᴜᴘᴘᴏʀᴛs Dɪʀᴇᴄᴛ Lɪɴᴋs & M3U8 Sᴛʀᴇᴀᴍs
-🔒 AES-128 HLS Sᴜᴘᴘᴏʀᴛᴇᴅ
-📤 Uʟᴛʀᴀ Fᴀsᴛ Tᴇʟᴇɢʀᴀᴍ Uᴘʟᴏᴀᴅs
+⚡ Aᴅᴠᴀɴᴄᴇᴅ Tᴇʟᴇɢʀᴀᴍ Dɪʀᴇᴄᴛ Lɪɴᴋ Uᴘʟᴏᴀᴅᴇʀ 🎬 Sᴛʀᴇᴀᴍ • Dᴏᴡɴʟᴏᴀᴅ • Uᴘʟᴏᴀᴅ Iɴsᴛᴀɴᴛʟʏ 📥 Sᴜᴘᴘᴏʀᴛs Dɪʀᴇᴄᴛ Lɪɴᴋs & M3U8 Sᴛʀᴇᴀᴍs 🔒 AES-128 HLS Sᴜᴘᴘᴏʀᴛᴇᴅ 📤 Uʟᴛʀᴀ Fᴀsᴛ Tᴇʟᴇɢʀᴀᴍ Uᴘʟᴏᴀᴅs
 
 ━━━━━━━━━━━━━━━━━━━
 
 📌 Wʜᴀᴛ Cᴀɴ I Dᴏ?
 
-• Uᴘʟᴏᴀᴅ Dɪʀᴇᴄᴛ Lɪɴᴋs Tᴏ Tᴇʟᴇɢʀᴀᴍ
-• Uᴘʟᴏᴀᴅ As Vɪᴅᴇᴏ / Fɪʟᴇ / Aᴜᴅɪᴏ
-• Sᴛʀᴇᴀᴍ Sᴜᴘᴘᴏʀᴛᴇᴅ Vɪᴅᴇᴏ Uᴘʟᴏᴀᴅs
-• Fᴀsᴛ Tᴇʟᴇɢʀᴀᴍ Uᴘʟᴏᴀᴅ Eɴɢɪɴᴇ
-• Aᴜᴛᴏ Mᴇᴅɪᴀ Pʀᴏᴄᴇssɪɴɢ
+• Uᴘʟᴏᴀᴅ Dɪʀᴇᴄᴛ Lɪɴᴋs Tᴏ Tᴇʟᴇɢʀᴀᴍ • Uᴘʟᴏᴀᴅ As Vɪᴅᴇᴏ / Fɪʟᴇ / Aᴜᴅɪᴏ • Sᴛʀᴇᴀᴍ Sᴜᴘᴘᴏʀᴛᴇᴅ Vɪᴅᴇᴏ Uᴘʟᴏᴀᴅs • Fᴀsᴛ Tᴇʟᴇɢʀᴀᴍ Uᴘʟᴏᴀᴅ Eɴɢɪɴᴇ • Aᴜᴛᴏ Mᴇᴅɪᴀ Pʀᴏᴄᴇssɪɴɢ
 
 ━━━━━━━━━━━━━━━━━━━
 
 🛠 Sᴜᴘᴘᴏʀᴛᴇᴅ Fᴏʀᴍᴀᴛs
 
-➤ MP4 • MKV • MP3
-➤ M3U8 / HLS Sᴛʀᴇᴀᴍs
-➤ Dɪʀᴇᴄᴛ Dᴏᴡɴʟᴏᴀᴅ URLs
-➤ Dᴏᴄᴜᴍᴇɴᴛs & Mᴇᴅɪᴀ Fɪʟᴇs
+➤ MP4 • MKV • MP3 ➤ M3U8 / HLS Sᴛʀᴇᴀᴍs ➤ Dɪʀᴇᴄᴛ Dᴏᴡɴʟᴏᴀᴅ URLs ➤ Dᴏᴄᴜᴍᴇɴᴛs & Mᴇᴅɪᴀ Fɪʟᴇs
 
 ━━━━━━━━━━━━━━━━━━━
 
 📥 Sᴇɴᴅ Mᴇ A Dɪʀᴇᴄᴛ Lɪɴᴋ Aɴᴅ I Wɪʟʟ Uᴘʟᴏᴀᴅ Iᴛ Tᴏ Tᴇʟᴇɢʀᴀᴍ As A Fɪʟᴇ / Vɪᴅᴇᴏ ⚡
 
 📖 Usᴇ /help Fᴏʀ Mᴏʀᴇ Dᴇᴛᴀɪʟs!
-"""
 
-<b>Join @url_anuj_bot"""
+<b>Jᴏɪɴ :</b> @url_anuj_bot """
+
 ADD_CAPTION_HELP = """
+
 📝 <b>Aᴅᴅ Cᴜsᴛᴏᴍ Cᴀᴘᴛɪᴏɴ Tᴏ Yᴏᴜʀ Fɪʟᴇs</b> ✨
 
 📤 Sᴇʟᴇᴄᴛ Oʀ Fᴏʀᴡᴀʀᴅ Aɴʏ <b>Tᴇʟᴇɢʀᴀᴍ Fɪʟᴇ / Vɪᴅᴇᴏ</b>
@@ -47,146 +37,145 @@ ADD_CAPTION_HELP = """
 💬 Tʜᴇɴ Rᴇᴘʟʏ Tᴏ Tʜᴀᴛ Fɪʟᴇ Wɪᴛʜ Tʜᴇ Tᴇxᴛ Yᴏᴜ Wᴀɴᴛ Tᴏ Sᴇᴛ As Cᴀᴘᴛɪᴏɴ.
 
 ⚡ Tʜᴇ Bᴏᴛ Wɪʟʟ Aᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ Aᴅᴅ Yᴏᴜʀ Tᴇxᴛ As Tʜᴇ Nᴇᴡ Cᴀᴘᴛɪᴏɴ 🤩
-"""
-    
-Ex: <a href='https://telegra.ph/file/198bcda5944f787373122.jpg'>See This!</a> 👇"""
-    INCORRECT_REQUEST = """
+
+Ex: <a href='https://telegra.ph/file/198bcda5944f787373122.jpg'>See This!</a> 👇 """
+
+INCORRECT_REQUEST = """
+
 ❗ <b>Iɴᴄᴏʀʀᴇᴄᴛ Rᴇǫᴜᴇsᴛ Fᴏʀᴍᴀᴛ</b>
 
 📌 Pʟᴇᴀsᴇ Mᴀᴋᴇ Sᴜʀᴇ Yᴏᴜ Sᴜʙᴍɪᴛ Yᴏᴜʀ Rᴇǫᴜᴇsᴛ Cᴏʀʀᴇᴄᴛʟʏ.
 
-💡 Cʜᴇᴄᴋ Tʜᴇ Ussᴀɢᴇ Gᴜɪᴅᴇ Oʀ Usᴇ /help Fᴏʀ Pʀᴏᴘᴇʀ Iɴsᴛʀᴜᴄᴛɪᴏɴs.
-"""
-    
-/help for more details!"""
-    DISPLAY_PROGRESS = """
+💡 Cʜᴇᴄᴋ Tʜᴇ Ussᴀɢᴇ Gᴜɪᴅᴇ Oʀ Usᴇ /help Fᴏʀ Pʀᴏᴘᴇʀ Iɴsᴛʀᴜᴄᴛɪᴏɴs. """
+
+DISPLAY_PROGRESS = """
+
 📥 <b>Dᴏᴡɴʟᴏᴀᴅɪɴɢ Vɪᴅᴇᴏ</b>
 
-╭━━━━❰ Pʀᴏɢʀᴇss ❱━━━━╮
-┣⪼ [{0}{1}] {2}%
-┣⪼ 🚀 Sᴘᴇᴇᴅ: <i>{6}/s</i>
-┣⪼ ⏱️ ETA: <i>{7}</i>
-┣⪼ 📦 Dᴏᴡɴʟᴏᴀᴅᴇᴅ: <i>{4} / {5}</i>
-┣⪼ 🗂️ Fɪʟᴇ: <i>{3}</i>
-╰━━━━━━━━━━━━━━━━╯
-"""
-    FORMAT_SELECTION = """
+╭━━━━❰ Pʀᴏɢʀᴇss ❱━━━━╮ ┣⪼ [{0}{1}] {2}% ┣⪼ 🚀 Sᴘᴇᴇᴅ: <i>{6}/s</i> ┣⪼ ⏱️ ETA: <i>{7}</i> ┣⪼ 📦 Dᴏᴡɴʟᴏᴀᴅᴇᴅ: <i>{4} / {5}</i> ┣⪼ 🗂️ Fɪʟᴇ: <i>{3}</i> ╰━━━━━━━━━━━━━━━━╯ """
+
+FORMAT_SELECTION = """
+
 🖼️ <b>Tʜᴜᴍʙɴᴀɪʟ Sᴇᴛᴜᴘ</b>
 
-📌 Iғ Yᴏᴜ Hᴀᴠᴇɴ'ᴛ Sᴇᴛ <a href='{}'>A Cᴜsᴛᴏᴍ Tʜᴜᴍʙɴᴀɪʟ</a> Yᴇᴛ,
-Yᴏᴜ Cᴀɴ Sᴇɴᴅ A Pʜᴏᴛᴏ Nᴏᴡ 🖼️
+📌 Iғ Yᴏᴜ Hᴀᴠᴇɴ'ᴛ Sᴇᴛ <a href='{}'>A Cᴜsᴛᴏᴍ Tʜᴜᴍʙɴᴀɪʟ</a> Yᴇᴛ, Yᴏᴜ Cᴀɴ Sᴇɴᴅ A Pʜᴏᴛᴏ Nᴏᴡ 🖼️
 
-⚡ Dᴏɴ'ᴛ Wᴀɴᴛ Tᴏ Sᴇᴛ Oɴᴇ?
-Nᴏ Pʀᴏʙʟᴇᴍ 😌
+⚡ Dᴏɴ'ᴛ Wᴀɴᴛ Tᴏ Sᴇᴛ Oɴᴇ? Nᴏ Pʀᴏʙʟᴇᴍ 😌
 
 🤖 Tʜᴇ Bᴏᴛ Wɪʟʟ Aᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ Gᴇɴᴇʀᴀᴛᴇ A Tʜᴜᴍʙɴᴀɪʟ Fʀᴏᴍ Tʜᴇ Vɪᴅᴇᴏ Fᴏʀ Yᴏᴜ.
-"""
+
+━━━━━━━━━━━━━━━━━━━
 
 𝗦𝗲𝗹𝗲𝗰𝘁 𝗔𝗻𝗱 𝗖𝗵𝗼𝘀𝗲 𝗬𝗼𝘂𝗿 𝗙𝗼𝗿𝗺𝗮𝘁 🎬
 
-⚠️ <b>Fɪʟᴇs Lᴀʀɢᴇʀ Tʜᴀɴ 2GB Aʀᴇ Nᴏᴛ Sᴜᴘᴘᴏʀᴛᴇᴅ.</b>
-"""
+⚠️ <b>Fɪʟᴇs Lᴀʀɢᴇʀ Tʜᴀɴ 2GB Aʀᴇ Nᴏᴛ Sᴜᴘᴘᴏʀᴛᴇᴅ.</b> """
 
 SET_CUSTOM_USERNAME_PASSWORD = """
+
 🔐 <b>Pʀᴇᴍɪᴜᴍ Vɪᴅᴇᴏ Dᴏᴡɴʟᴏᴀᴅ</b>
 
-Iғ Tʜᴇ Vɪᴅᴇᴏ Rᴇǫᴜɪʀᴇs Lᴏɢɪɴ Cʀᴇᴅᴇɴᴛɪᴀʟs,
-Sᴇɴᴅ Tʜᴇ Dᴇᴛᴀɪʟs Iɴ Tʜᴇ Fᴏʟʟᴏᴡɪɴɢ Fᴏʀᴍᴀᴛ 👇
+Iғ Tʜᴇ Vɪᴅᴇᴏ Rᴇǫᴜɪʀᴇs Lᴏɢɪɴ Cʀᴇᴅᴇɴᴛɪᴀʟs, Sᴇɴᴅ Tʜᴇ Dᴇᴛᴀɪʟs Iɴ Tʜᴇ Fᴏʟʟᴏᴡɪɴɢ Fᴏʀᴍᴀᴛ 👇
 
-<code>URL | filename | username | password</code>
-"""
+<code>URL | filename | username | password</code> """
 
 DOWNLOAD_START = """
+
 📥 <b>Fɪʟᴇ Dᴇᴛᴇᴄᴛᴇᴅ</b>
 
 🗂️ <i>{}</i>
 
-⚡ Sᴛᴀʀᴛɪɴɢ Dᴏᴡɴʟᴏᴀᴅ...
-"""
-    
+⚡ Sᴛᴀʀᴛɪɴɢ Dᴏᴡɴʟᴏᴀᴅ... """
+
+DOWNLOADING = """
+
 <b>Dᴏᴡɴʟᴏᴀᴅɪɴɢ Tᴏ Mʏ Sᴇʀᴠᴇʀ... 📥</b>
 
-Pʟᴇᴀsᴇ Wᴀɪᴛ, Uᴘʟᴏᴀᴅɪɴɢ Wɪʟʟ Sᴛᴀʀᴛ As Sᴏᴏɴ As Pᴏssɪʙʟᴇ 😎"""
+Pʟᴇᴀsᴇ Wᴀɪᴛ, Uᴘʟᴏᴀᴅɪɴɢ Wɪʟʟ Sᴛᴀʀᴛ As Sᴏᴏɴ As Pᴏssɪʙʟᴇ 😎 """
 
 UPLOAD_START = "<b>📤 Uᴘʟᴏᴀᴅɪɴɢ Tᴏ Tᴇʟᴇɢʀᴀᴍ... ⚡</b>"
 
 RCHD_TG_API_LIMIT = """<b>❗Fɪʟᴇ Cᴏᴜʟᴅɴ'ᴛ Bᴇ Uᴘʟᴏᴀᴅᴇᴅ❗</b>
 
 😔 Sᴏʀʀʏ, I Cᴀɴɴᴏᴛ Uᴘʟᴏᴀᴅ Fɪʟᴇs Lᴀʀɢᴇʀ Tʜᴀɴ 2GB Dᴜᴇ Tᴏ Tᴇʟᴇɢʀᴀᴍ API Lɪᴍɪᴛᴀᴛɪᴏɴs.
-"""
 
-<b>▫️File detected:</b> <i>{}</i>
-<b>▫️Downloaded:</b> <i>in {} seconds</i>
-<b>▫️Detected file size:</b> <i>{}</i>"""
-    UNKNOWN_ERROR = """<b>❗UNKNOWN ERROR❗</b>
-I don't know what just happened 🙃
-But I'm going to find out and fix it as soon as possible 🧐"""
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "👍 Thanks for using @knoxprojects."
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = """<i>◽ Downloaded in <b>{} seconds</b></i>
-<i>◽ Uploaded in <b>{} seconds</b></i>"""
-    SAVED_CUSTOM_THUMB_NAIL = "◽ Custom video/file thumbnail saved. This image will be used in the video/file."
-    DEL_ETED_CUSTOM_THUMB_NAIL = "◽ Custom thumbnail cleared succesfully."
-    CUSTOM_CAPTION_UL_FILE = "{}"
-    NO_FILE_FOUND = """<b>❗I couldn't find any video/file❗</b>
-Go check if you can access the content in the URL from your browser first!"""
-    NO_VOID_FORMAT_FOUND = """<b>❗SOMETHING WENT WRONG❗</b>
-I think you have entered an unaccessible URL or a private URL (which only works with your IP).
+<b>▫️Fɪʟᴇ Dᴇᴛᴇᴄᴛᴇᴅ:</b> <i>{}</i> <b>▫️Dᴏᴡɴʟᴏᴀᴅᴇᴅ:</b> <i>ɪɴ {} sᴇᴄᴏɴᴅs</i> <b>▫️Dᴇᴛᴇᴄᴛᴇᴅ Fɪʟᴇ Sɪᴢᴇ:</b> <i>{}</i> """
 
-<b>🚨Additional info:</b>
-{}"""
-    HELP_USER = """<b>📖 Hᴏᴡ Tᴏ Usᴇ Mᴇ?</b> 🤔
+UNKNOWN_ERROR = """<b>❗UNKNOWN ERROR❗</b>
+
+I Dᴏɴ'ᴛ Kɴᴏᴡ Wʜᴀᴛ Jᴜsᴛ Hᴀᴘᴘᴇɴᴇᴅ 🙃 Bᴜᴛ I'ᴍ Gᴏɪɴɢ Tᴏ Fɪɴᴅ Oᴜᴛ Aɴᴅ Fɪx Iᴛ As Sᴏᴏɴ As Pᴏssɪʙʟᴇ 🧐 """
+
+AFTER_SUCCESSFUL_UPLOAD_MSG = "👍 Tʜᴀɴᴋs Fᴏʀ Usɪɴɢ @url_anuj_bot"
+
+AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = """
+
+<i>◽ Dᴏᴡɴʟᴏᴀᴅᴇᴅ Iɴ <b>{} Sᴇᴄᴏɴᴅs</b></i> <i>◽ Uᴘʟᴏᴀᴅᴇᴅ Iɴ <b>{} Sᴇᴄᴏɴᴅs</b></i> """
+
+SAVED_CUSTOM_THUMB_NAIL = "◽ Cᴜsᴛᴏᴍ Vɪᴅᴇᴏ/Fɪʟᴇ Tʜᴜᴍʙɴᴀɪʟ Sᴀᴠᴇᴅ Sᴜᴄᴄᴇssғᴜʟʟʏ."
+
+DEL_ETED_CUSTOM_THUMB_NAIL = "◽ Cᴜsᴛᴏᴍ Tʜᴜᴍʙɴᴀɪʟ Cʟᴇᴀʀᴇᴅ Sᴜᴄᴄᴇssғᴜʟʟʏ."
+
+CUSTOM_CAPTION_UL_FILE = "{}"
+
+NO_FILE_FOUND = """<b>❗I Cᴏᴜʟᴅɴ'ᴛ Fɪɴᴅ Aɴʏ Vɪᴅᴇᴏ/Fɪʟᴇ❗</b>
+
+Gᴏ Cʜᴇᴄᴋ Iғ Yᴏᴜ Cᴀɴ Aᴄᴄᴇss Tʜᴇ Cᴏɴᴛᴇɴᴛ Iɴ Tʜᴇ URL Fʀᴏᴍ Yᴏᴜʀ Bʀᴏᴡsᴇʀ Fɪʀsᴛ! """
+
+NO_VOID_FORMAT_FOUND = """<b>❗SOMETHING WENT WRONG❗</b>
+
+I Tʜɪɴᴋ Yᴏᴜ Hᴀᴠᴇ Eɴᴛᴇʀᴇᴅ Aɴ Uɴᴀᴄᴄᴇssɪʙʟᴇ URL Oʀ A Pʀɪᴠᴀᴛᴇ URL.
+
+<b>🚨 Aᴅᴅɪᴛɪᴏɴᴀʟ Iɴғᴏ:</b> {} """
+
+HELP_USER = """<b>📖 Hᴏᴡ Tᴏ Usᴇ Mᴇ?</b> 🤔
 
 Fᴏʟʟᴏᴡ Tʜᴇsᴇ Sɪᴍᴘʟᴇ Sᴛᴇᴘs 👇
-"""
-    
-<b>1. Sᴇɴᴅ URL</b>
+
+<b>1️⃣ Sᴇɴᴅ URL</b>
 
 ✨ <b>Cᴜsᴛᴏᴍ Cᴀᴘᴛɪᴏɴ Fᴏʀ Vɪᴅᴇᴏ / Fɪʟᴇ</b>
 
-Iғ Yᴏᴜ Wᴀɴᴛ Tᴏ Sᴇᴛ A Cᴜsᴛᴏᴍ Cᴀᴘᴛɪᴏɴ Oɴ Yᴏᴜʀ Vɪᴅᴇᴏ Oʀ Fɪʟᴇ,
 Sᴇɴᴅ Tʜᴇ Lɪɴᴋ Iɴ Tʜᴇ Fᴏʟʟᴏᴡɪɴɢ Fᴏʀᴍᴀᴛ 👇
 
 <b>Link * Caption</b>
 
-<i>[Sᴇᴘᴀʀᴀᴛᴇ Tʜᴇ Lɪɴᴋ Aɴᴅ Cᴀᴘᴛɪᴏɴ Wɪᴛʜ "*" Mᴀʀᴋ]</i>
+<b>✅ Cᴏʀʀᴇᴄᴛ Exᴀᴍᴘʟᴇ:</b> <code>https://www.website.com/video.mp4 * My Caption</code>
 
-<u>⚠️ Iᴛ Is Iᴍᴘᴏʀᴛᴀɴᴛ Tᴏ Gɪᴠᴇ Sᴘᴀᴄᴇs Bᴇғᴏʀᴇ Aɴᴅ Aғᴛᴇʀ "*"</u>
+<b>❌ Wʀᴏɴɢ Exᴀᴍᴘʟᴇ:</b> <code>https://www.website.com/video.mp4*My Caption</code>
 
-<b>✅ Cᴏʀʀᴇᴄᴛ Exᴀᴍᴘʟᴇ:</b>
+━━━━━━━━━━━━━━━━━━━
 
-<code>https://www.website.com/video.mp4 * My Custom Caption</code>
+<b>2️⃣ Sᴇɴᴅ Cᴜsᴛᴏᴍ Tʜᴜᴍʙɴᴀɪʟ</b>
 
-<b>❌ Wʀᴏɴɢ Exᴀᴍᴘʟᴇ:</b>
+⚡ Tʜɪs Sᴛᴇᴘ Is Oᴘᴛɪᴏɴᴀʟ.
 
-<code>https://www.website.com/video.mp4*My Caption</code>
+🔹 Iғ Yᴏᴜ Dᴏɴ'ᴛ Sᴇɴᴅ A Tʜᴜᴍʙɴᴀɪʟ, Tʜᴇ Bᴏᴛ Wɪʟʟ Gᴇɴᴇʀᴀᴛᴇ Oɴᴇ Aᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ.
 
-The caption/text you type will be automatically set as the custom name of the uploaded file 😎
+🔹 Usᴇ /delthumbnail Tᴏ Dᴇʟᴇᴛᴇ Tʜᴇ Sᴀᴠᴇᴅ Tʜᴜᴍʙɴᴀɪʟ.
 
-<i><b>Note:</b> You can change/add any caption later if you want as explained in the end.</i>
+━━━━━━━━━━━━━━━━━━━
 
-<b>2. Then send custom thumbnail when asked while uploading the URL</b> (This step is optional) 
+<b>3️⃣ Sᴇʟᴇᴄᴛ Fᴏʀᴍᴀᴛ</b>
 
-🔹 It means it is not necessary to send an image to include as an thumbnail.
-If you don't send a thumbnail the video/file will be uploaded with an auto generated thumbnail from the video.
-<i>(The thumbnail you send will be used for your next uploads)</i>
+<u>Video-option</u> ➠ Uᴘʟᴏᴀᴅ As Vɪᴅᴇᴏ <u>File-option</u> ➠ Uᴘʟᴏᴀᴅ As Fɪʟᴇ
 
-🔹 Press /delthumbnail if you want to delete the previously saved thumbnail.
-<i>(Then the video will be uploaded with an auto-genarated thumbnail)</i>
+━━━━━━━━━━━━━━━━━━━
 
-<b>3. Select the button</b>
+<b>👉 Sᴘᴇᴄɪᴀʟ Fᴇᴀᴛᴜʀᴇ</b>
 
-  <u>Video-option</u>: Give video/file in video format
-  <u>File-option</u>: Give video/file in file format
-   
-<b>👉 Special feature: Set caption to any file you want! ✨</b>
+🔹 Rᴇᴘʟʏ Tᴏ Aɴʏ Tᴇʟᴇɢʀᴀᴍ Fɪʟᴇ Wɪᴛʜ Yᴏᴜʀ Tᴇxᴛ Tᴏ Aᴅᴅ A Cᴜsᴛᴏᴍ Cᴀᴘᴛɪᴏɴ 🤩
 
-🔹 Select an uploaded file/video or forward me <b>Any Telegram File</b> and just write the text you want to be on the file <b>as a reply to the file</b> by selecting it (as replying to a message) and the text you wrote will be attached as caption! 🤩
+Ex: <a href='https://telegra.ph/file/198bcda5944f787373122.jpg'>Send Like This! It's Easy</a> 🥳 """
 
-Ex: <a href='https://telegra.ph/file/198bcda5944f787373122.jpg'>Send Like This! It's Easy</a> 🥳"""
-    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Reply /generatecustomthumbnail to a media album, to generate custom thumbail."
-    ERR_ONLY_TWO_MEDIA_IN_ALBUM = """Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
-You can use /rename command after receiving file to rename it with custom thumbnail support.
-"""
-    CANCEL_STR = "Process Cancelled."
-    ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds."
-    SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
+REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Rᴇᴘʟʏ /generatecustomthumbnail Tᴏ A Mᴇᴅɪᴀ Aʟʙᴜᴍ Tᴏ Gᴇɴᴇʀᴀᴛᴇ Tʜᴜᴍʙɴᴀɪʟ."
+
+ERR_ONLY_TWO_MEDIA_IN_ALBUM = """Mᴇᴅɪᴀ Aʟʙᴜᴍ Sʜᴏᴜʟᴅ Cᴏɴᴛᴀɪɴ Oɴʟʏ Tᴡᴏ Pʜᴏᴛᴏs.
+
+Pʟᴇᴀsᴇ Rᴇ-Sᴇɴᴅ Tʜᴇ Mᴇᴅɪᴀ Aʟʙᴜᴍ Aɢᴀɪɴ.
+
+Yᴏᴜ Cᴀɴ Usᴇ /rename Cᴏᴍᴍᴀɴᴅ Aғᴛᴇʀ Rᴇᴄᴇɪᴠɪɴɢ Tʜᴇ Fɪʟᴇ. """
+
+CANCEL_STR = "Pʀᴏᴄᴇss Cᴀɴᴄᴇʟʟᴇᴅ."
+
+ZIP_UPLOADED_STR = "Uᴘʟᴏᴀᴅᴇᴅ {} Fɪʟᴇs Iɴ {} Sᴇᴄᴏɴᴅs."
+
+SLOW_URL_DECED = "Tʜɪs URL Is Vᴇʀʏ Sʟᴏᴡ 😴\n\nPʟᴇᴀsᴇ Tʀʏ A Fᴀsᴛᴇʀ Dɪʀᴇᴄᴛ Lɪɴᴋ Fᴏʀ Sᴍᴏᴏᴛʜ Uᴘʟᴏᴀᴅs ⚡"
